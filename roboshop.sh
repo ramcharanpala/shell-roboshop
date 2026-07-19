797d0748fb7a2cfbf5aa9f777d1330b520d93403
ramcharanpalagit config --global user.mail ramcharanpala1257@gmail.com <ramcharanpala1257@gmail.comgit config --global user.name ramcharanpala>
2026-07-19 15:52:49 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0f1b4f5f9a6d240f1"
-
+ZONE_ID="Z0065890B2YYRVM9H4E0"
+DOMAIN_NAME="ram86s.fun"
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
 
     echo "$instance: $IP"
 
+     aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
