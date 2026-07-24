8ebd1b7c49707f145efaace8cf6cd477ae54b7e0
ramcharanpalagit config --global user.mail ramcharanpala1257@gmail.com <ramcharanpala1257@gmail.comgit config --global user.name ramcharanpala>
2026-07-24 17:24:21 +0530

    roboshop

---
+++ redis.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOCAL_HOST=redis.ram86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script execution started at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: please run this script in root privelege"
+    exit 1
+fi
+
+VALIDATION(){
+    if [ $1 -ne 0 ]; then
+       echo -e "$2 ... $R FAILURE $N | tee -a &>>LOG_FILE
+       exit 1
+    else
+       echo -e "$2 ... $G SUCCESS $N | tee -a &>>LOG_FILE 
+    fi
+}
+
+dnf module disable redis -y &>>LOG_FILE
+VALIDATION $? "disabling default redis" 
+
+dnf module enable redis:7 -y &>>LOG_FILE
+VALIDATION $? "enable redis:7" 
+
+dnf install redis -y  &>>LOG_FILE
+VALIDATION $? "install redis" 
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATION $? "allowing remote connections to the redis" 
+
+systemctl enable redis &>>LOG_FILE
+VALIDATION $? "enable redis" 
+
+systemctl start redis &>>LOG_FILE
+VALIDATION $? "start redis" 
