11e421af7edb7ad4e1ac504daab39a3a80e6765a
ramcharanpalagit config --global user.mail ramcharanpala1257@gmail.com <ramcharanpala1257@gmail.comgit config --global user.name ramcharanpala>
2026-07-27 22:19:36 +0530

    roboshop

--- shipping.sh
+++ shipping.sh
@@ -38,7 +38,7 @@ if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop"
     VALIDATION $? "creating system user"
 else
-    echo "system user already exists ... $Y SKIPPING $N"
+    echo -e "system user already exists ... $Y SKIPPING $N"
 fi
 
 mkdir -p /app 
@@ -68,9 +68,9 @@ dnf install mysql -y &>>$LOG_FILE
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-   mysql -h  $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql  &>>$LOG_FILE
-   mysql -h  $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
-   mysql -h  $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql  &>>$LOG_FILE
+    mysql -h  $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql  &>>$LOG_FILE
+    mysql -h  $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h  $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql  &>>$LOG_FILE
 else
    echo -e "shipping data is already loaded ... $Y SKIPPING $N"
 fi
